403d3553540bcc69fcef8d352c5d8ea2a75353de
GreenEnergy86 <31546136+moon200702@users.noreply.github.com>
2026-07-08 01:06:16 +0000

    change

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const xmlOutput = document.getElementById('xmlOutput');
 async function init() {
   try {
     const [productCsv, channelCsv] = await Promise.all([
-      fetch('./office_product_id.csv').then((response) => response.text()),
-      fetch('./office_channel.csv').then((response) => response.text())
+      fetch('office_product_id.csv').then((response) => response.text()),
+      fetch('office_channel.csv').then((response) => response.text())
     ]);
 
     productRows = parseCsv(productCsv);
